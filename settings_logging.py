0d46d5c5d2b2719b8f63ae93fd664462a86f3e47
Ubuntu <ubuntu@ip-172-31-31-62.ap-northeast-2.compute.internal>
2018-06-10 01:50:11 +0000

    add settings_logging.py

---
+++ settings_logging.py
@@ -0,0 +1,40 @@
+
+import os
+
+
+# Logging
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/debug.log',
+	    'formatter': 'verbose',
+        },
+	'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+	'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
+
